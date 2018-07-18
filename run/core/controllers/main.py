513140bd11ea4011f75036349d17ba5e0e428749
Matthew Muccio <me@matthewmuccio.com>
2018-07-18 16:03:41 -0400

    Fixes bug where the index page was not being routing to correctly.

--- run/core/controllers/main.py
+++ run/core/controllers/main.py
@@ -11,7 +11,16 @@ controller = Blueprint("main", __name__)
 
 @controller.route("/", methods=["GET"])
 def show_index():
-    return render_template("market-home-index.html")
+    item = model.get_item_data("bicycle")
+    # If the item name exists in the DB:
+    if item:
+        name = item[1]      # bicycle
+        price = item[2]     # $25.00
+        picture = item[3]   # photo-1531857475897-48f2102b7566.jpeg
+        return render_template("item.html", \
+                                name=name, \
+                                price=price, \
+                                picture=picture)
 
 @controller.route("/<item_name>", methods=["GET"])
 def show_item(item_name):
@@ -27,7 +36,4 @@ def show_item(item_name):
                                 picture=picture)
     # If the item name does not exist in the DB:
     else:
-        return render_template("item-not-found.html", \
-                                name="name", \
-                                price="price", \
-                                picture="picture")
+        return render_template("item-not-found.html")
